f766cae4c8a7cc943ab93419d61cbf200d1ab8e6
Joker701 <fseverino@gmail.com>
2015-02-18 14:57:29 -0600

    begin setup of user auth

---
+++ app/database/migrations/2014_11_04_043426_create_users_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateUsersTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('users', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('username');
+			$table->string('firstname');
+			$table->string('lastname');
+			$table->string('email');
+			$table->string('password');
+			$table->string('telephone');
+			$table->string('avatar');
+			$table->boolean('admin')->default(2);
+			$table->string('remember_token')->nullable();
+			$table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+			$table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
+
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('users');
+	}
+
+}
