2fee29577ff0c053731f538e0b792a64ad029475
Joker701 <fseverino@gmail.com>
2015-02-19 11:57:11 -0600

    composer update changes

--- _ide_helper_models.php
+++ _ide_helper_models.php
@@ -12,6 +12,44 @@ namespace {
 /**
  * User
  *
+ * @property integer $id 
+ * @property string $username 
+ * @property string $firstname 
+ * @property string $lastname 
+ * @property string $email 
+ * @property string $password 
+ * @property string $telephone 
+ * @property string $avatar 
+ * @property boolean $admin 
+ * @property string $remember_token 
+ * @property \Carbon\Carbon $created_at 
+ * @property \Carbon\Carbon $updated_at 
+ * @property boolean $stripe_active 
+ * @property string $stripe_id 
+ * @property string $stripe_subscription 
+ * @property string $stripe_plan 
+ * @property string $last_four 
+ * @property \Carbon\Carbon $trial_ends_at 
+ * @property \Carbon\Carbon $subscription_ends_at 
+ * @method static \Illuminate\Database\Query\Builder|\User whereId($value)
+ * @method static \Illuminate\Database\Query\Builder|\User whereUsername($value)
+ * @method static \Illuminate\Database\Query\Builder|\User whereFirstname($value)
+ * @method static \Illuminate\Database\Query\Builder|\User whereLastname($value)
+ * @method static \Illuminate\Database\Query\Builder|\User whereEmail($value)
+ * @method static \Illuminate\Database\Query\Builder|\User wherePassword($value)
+ * @method static \Illuminate\Database\Query\Builder|\User whereTelephone($value)
+ * @method static \Illuminate\Database\Query\Builder|\User whereAvatar($value)
+ * @method static \Illuminate\Database\Query\Builder|\User whereAdmin($value)
+ * @method static \Illuminate\Database\Query\Builder|\User whereRememberToken($value)
+ * @method static \Illuminate\Database\Query\Builder|\User whereCreatedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|\User whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|\User whereStripeActive($value)
+ * @method static \Illuminate\Database\Query\Builder|\User whereStripeId($value)
+ * @method static \Illuminate\Database\Query\Builder|\User whereStripeSubscription($value)
+ * @method static \Illuminate\Database\Query\Builder|\User whereStripePlan($value)
+ * @method static \Illuminate\Database\Query\Builder|\User whereLastFour($value)
+ * @method static \Illuminate\Database\Query\Builder|\User whereTrialEndsAt($value)
+ * @method static \Illuminate\Database\Query\Builder|\User whereSubscriptionEndsAt($value)
  */
 	class User {}
 }
